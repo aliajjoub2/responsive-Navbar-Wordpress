45104f2f167c3ae24ef4e04dbbbca1c8f7f1170b
aliajjoub2 <83879405+aliajjoub2@users.noreply.github.com>
2022-06-28 13:39:20 +0200

    first commit

---
+++ js/navbarwordpress.js
@@ -0,0 +1,54 @@
+/*  // add i after mainmenu
+ function addElement () {
+    // create a new div element
+    const newDiv = document.createElement("span");
+  
+    // and give it some content
+    const newContent = document.createTextNode("here new");
+  
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);
+  
+    // add the newly created element and its content into the DOM
+    const currentDiv = document.getElementById("menu-item-64");
+    document.body.insertBefore(newDiv, currentDiv);
+  }
+  addElement (); */
+
+/*   var tag = document.createElement("p");
+   var text = document.createTextNode("Tutorix is the best e-learning platform");
+   tag.appendChild(text);
+   var element = document.getElementById("menu-item-64");
+   element.appendChild(tag); */
+
+/* 
+ // sidebar open close js code
+ let navLinks = document.querySelector(".nav-links");
+ let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+ let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+ menuOpenBtn.onclick = function() {
+ navLinks.style.left = "0";
+ }
+ menuCloseBtn.onclick = function() {
+ navLinks.style.left = "-100%";
+ }
+
+
+ // sidebar submenu open close js code
+ let htmlcssArrow = document.querySelector(".OurServiice");
+ htmlcssArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+ }
+ let moreArrow = document.querySelector(".more-arrow");
+ moreArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+ }
+ let jsArrow = document.querySelector(".euroNavbar");
+ jsArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+ }
+ let langArrow = document.querySelector(".langMobil");
+ langArrow.onclick = function() {
+ navLinks.classList.toggle("show4");
+ } */
+
