d441d53c45500487aecc99aa0cdc54ae99286972
aliajjoub2 <83879405+aliajjoub2@users.noreply.github.com>
2022-07-01 13:07:13 +0200

    add function size change & change css & add class classContainer

--- js/navbarwordpress.js
+++ js/navbarwordpress.js
@@ -1,119 +1,66 @@
-// document.querySelector('div').insertAdjacentHTML('afterBegin','<p>Second paragraph in the code, but should become first.</p>')
 
-// count tag hwho has ClassName menu-item-has-children
-clannNameNumber = document.getElementsByClassName(
-  "menu-item-has-children"
-).length;
+// define how many elemnet have class name 'menu-item-has-children'
+clannNameNumber = document.getElementsByClassName("menu-item-has-children").length;
+// add i down  to every  ClassName menu-item-has-children
 for (let i = 0; i < clannNameNumber; i++) {
-  // add i to ClassName menu-item-has-children
   document.getElementsByClassName("menu-item-has-children")[i].innerHTML +=
     "<i>&#xfe40;</i>";
 }
 
 let menu = document.getElementsByClassName("menu")[0];
+let mainMenuId = document.getElementsByClassName("menu")[0].id;
+let divClass = mainMenuId + "-container";
+let divClassSelect= document.getElementsByClassName(divClass);
 
-mainMenuId = document.getElementsByClassName("menu")[0].id;
-divClass = mainMenuId + "-container";
-
-  /*  menu.innerHTML = "<p id ='closeBtn'>X</p>" + menu.innerHTML;
-    menu.innerHTML += "<i id='menuSympol'>&#9776;</i>";
-    menu.innerHTML += "<h2 id='MobileHome'>الموقع</h2>"; */
-
-/*   function myFunction() {
-    const myMenu = document.getElementById(mainMenuId);
-    let MobileHome = "<h2 id='MobileHome'>الموقع</h2>";
-    let menuSympol = "<i id='menuSympol'>&#9776;</i>";
-    let closeBtn = "<p id ='closeBtn'>X</p>";
-    myMenu.insertAdjacentHTML("afterend", MobileHome);
-    myMenu.insertAdjacentHTML("afterend", menuSympol);
-    myMenu.insertAdjacentHTML("afterend", closeBtn);
-   
-  };
-myFunction(); */
-
-divClassSelect= document.getElementsByClassName(divClass);
-divClassSelect[0].innerHTML+= "<span id ='closeBtn'>XXXX</span>";
+// add span close botton and Menu Sympol and MobileHome to div menu
+divClassSelect[0].innerHTML+= "<span id ='closeBtn'>X</span>";
 divClassSelect[0].innerHTML+= "<span id='menuSympol'>&#9776;</span>";
-divClassSelect[0].innerHTML+= "<span id='MobileHome'>الموقع</span>";
-
-/* const para = document.createElement("p");
-para.innerHTML = "القائمة";
-
-// Append to another element:
-document.getElementsByClassName(divClass)[0].appendChild(para); */
+divClassSelect[0].innerHTML+= "<span id='MobileHome'>Home Page</span>";
 
 
-
-// menu.innerHTML += "";
 
 let menuSympol = document.getElementById("menuSympol");
 let MobileHome = document.getElementById("MobileHome");
 let closeBtn = document.getElementById("closeBtn");
 let MenuIDTag= document.getElementById(mainMenuId);
 
-/* let menuItem = document.getElementsByClassName(
-  "menu-item-type-post_type"
-).length; */
-
-
-
+// function  close the menu wehn click bottom closeBtn 
 closeBtn.onclick = function () {
     MenuIDTag.style.right = "-100%";
   menuSympol.style.display = "block";
   MobileHome.style.display = "block";
-
- /*  for (let i = 0; i < menuItem; i++) {
-    document.getElementsByClassName("menu-item-type-post_type")[
-      i
-    ].style.display = "none";
-  } */
-
   closeBtn.style.display = "none";
-  /* let w = window.innerWidth;
-  if (w > 800) {
-    document.getElementsByClassName("menu-item-type-post_type")[
-      i
-    ].style.display = "flex";
-    menu.style.display = "flex";
-    menuSympol.style.display = "none";
-    MobileHome.style.display = "none";
-    closeBtn.style.display = "none";
-  } */
 };
+
+// function change css menu and all bottom when change the window size
 function myFunction(x) {
-    if (x.matches) { // If media query matches
-        MenuIDTag.style.backgroundColor = "black";
+    if (x.matches) { // If media query matches     
         MenuIDTag.style.right = "-300";
-        MenuIDTag.style.width = "auto";
+        MenuIDTag.style.display = "none";
         
-    } else {
-        
-        MenuIDTag.style.backgroundColor = "rot";
-        /* MenuIDTag.style.display = "block"; */
-        /* MenuIDTag.style.flexDirection = "column"; */
-      
-       
-        
+    } else { 
+        MenuIDTag.style.right = "0";
+        MenuIDTag.style.width = "100%";
+        MenuIDTag.style.display = "flex";
     }
   }
   
-  var x = window.matchMedia("(max-width: 800px)")
+  var x = window.matchMedia("(max-width: 1025px)")
   myFunction(x) // Call listener function at run time
   x.addListener(myFunction);
 
+  // function click menu 
   menuSympol.onclick = function () {
-
+    MenuIDTag.style.display = "block";
     MenuIDTag.style.right = "0";
   menu.style.color="black"
   menuSympol.style.display = "none";
   MobileHome.style.display = "none"; 
 
   closeBtn.style.display = "block";
-  /*      
-        for (let i = 0; i < menuItem; i++) {
-        // add i to ClassName menu-item-has-children
-        document.getElementsByClassName('menu-item-type-post_type')[i].style.display="block";
-             
-    } */
-  
+
 };
+
+// add class ClassContainer to main Div of the menu
+var element = document.getElementsByClassName(divClass)[0];
+element.classList.add("ClassContainer");
